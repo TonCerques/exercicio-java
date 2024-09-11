b53b78349e6d855bbe92348aceceb9199f69aa7a
TonCerques <zcoltyy@gmail.com>
2024-09-11 14:39:18 -0300

    Feat: Aprovação de Empréstimo

--- src/Banco.java
+++ src/Banco.java
@@ -5,40 +5,55 @@ public class Banco {
    public static void main(String[] args) {
     
     Scanner ler = new Scanner(System.in);
-    int emprestimo;
-    int parcelas;
-    int salario;
-    int valor;
+    double emprestimo = 0, salario = 0, valorparcela = 0, valorlimite = 0;
+    int parcelas = 0;
+    boolean aprovadoParcelas=false;
+    boolean aprovadoSalario =false;
 
-
+    
     System.out.println("Você deseja realizar um empréstimo no Serasa? Diga o valor de empréstimo que deseja:");
     emprestimo = ler.nextInt();
+    
+    while (aprovadoParcelas == false){
 
+    System.out.println();    
     System.out.println("Número de parcelas que deseja está pagando o empréstimo:");
     parcelas = ler.nextInt();
+        if (parcelas > 12){
+            System.out.println();
+            System.out.println("O limite de parcelas é de 12 vezes com 30% de juros!");
+        } else if (parcelas <= 12) {
+            System.out.println();
+            System.out.println("Está dentro do limite desejado!");
+            aprovadoParcelas = true;
+        }
+    } // FIM WHILE
+
+    while (aprovadoSalario == false) {
 
     System.out.println();
     System.out.println("Sua renda salarial:");
     salario = ler.nextInt();
+        if (salario < 1200){
+            System.out.println();
+            System.out.println("A gente não faz empréstimo pra liso, se saia mizera");
+        } else {
+            System.out.println();
+            System.out.println("Está dentro das nossas regulamentações lhe oferecer o empréstimo.");
+            aprovadoSalario = true;
+        }
+    } // FIM WHILE
 
-    if (parcelas >= 12){
-        System.out.println("O limite de parcelas é de 12 vezes sem juros!");
-    } else if (parcelas <= 12) {
-        System.out.println("Está dentro do limite desejado!");
-    }
-
-    if (salario <= 1200){
-        System.out.println("A gente não faz empréstimo pra liso, se saia mizera");
-    } else if (salario >= 1200) {
-        System.out.println("Está dentro das nossas regulamentações lhe oferecer o empréstimo.");
-    }
+     System.out.println();
+        valorparcela = emprestimo / parcelas;
+        valorlimite = salario * 0.30;
+        if (valorlimite <= salario) {
+            System.out.println("Empréstimo aprovado!");
+        } else {
+            System.out.println("Empréstimo reprovado!");
+        }
 
 
+    }// fim main
+    }//fim class
 
-    }
-
-
-
-        
-    }
-}
